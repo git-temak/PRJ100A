ce06d67dabb020d6d675b6449a75e57af2fbb1fc
Timi Makinde <makindeoluwatimilehin@gmail.com>
2024-05-11 00:34:51 +0100

    updates
    - catch errors and alert user
    - styling updates
    - reduce redundant code

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const output = document.getElementById('output');
 
-function appendToOutput(value) {
+function processBtn(value) {
     output.value += value;
 }
 
-function performOperation(operator) {
-    output.value += operator;
-}
-
 function calculateResult() {
-    var result = eval(output.value);
-    output.value = parseFloat(result.toFixed(2));
+    try {
+        let result = eval(output.value);
+        let finalRes = parseFloat(result.toFixed(10));
+        if (isNaN(finalRes) || !isFinite(finalRes)) {
+            throw new Error('Invalid calculation');
+        }
+        output.value = finalRes;
+    } catch (error) {
+        alert('Invalid calculation');
+    }
 }
